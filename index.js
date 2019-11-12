f060e36076813cd4c474535f872dd753a1db4ad6
Douglas Eccker <douglaseccker@hotmail.com>
2019-11-11 21:50:00 -0300

    adicionar CORS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const app = require('express')()
 const consign = require('consign')
 const db = require('./config/db')
+var cors = require('cors');
 
 app.db = db
+
+app.use(cors());
 
 consign()
     .then('./models')
@@ -13,6 +16,6 @@ consign()
     .then('./config/routes.js')
     .into(app)
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('Backend executando...')
 })
