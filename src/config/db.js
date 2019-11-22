7e5d057104c3aa09ded1b5a44b036495af48c501
luan.lazzari <luanlazzari@gmail.com>
2019-11-22 00:56:03 -0300

    Implementada conexão com Mongo Atlas

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
+const { mongoURI } = require('../../.env')
 
-mongoose.connect('mongodb://localhost/nutrysis', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        const msg = "Conectado ao banco"
+        console.log('\x1b[42m%s\x1b[37m', msg, '\x1b[0m')
+    })
     .catch(e => {
         const msg = 'Não foi possível conectar com o MongoDB!'
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
